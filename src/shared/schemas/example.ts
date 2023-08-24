effe31596e44b73246f946441238ebdd6006477d
lumartinezz <107443972+lumartinezz@users.noreply.github.com>
2023-08-24 15:12:50 -0300

    resolve todo list

--- src/shared/schemas/example.ts
+++
@@ -1,4 +0,0 @@
-import * as yup from "yup";
-
-export const helloSchema = yup.object({ text: yup.string().required() });
-// TODO: REMOVE EXAMPLE
--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,4 +1,2 @@
 export * from "./api";
 export * from "./brands";
-export * from "./example";
-// TODO: REMOVE EXAMPLE
